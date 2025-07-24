4717c3c8efef38965171afecc80a2469a2221c83
brinersam <vladbudyachenko@mail.ru>
2025-07-24 23:35:57 +0300

    Updated to 1.0.2 Fixed pagedList being Ienumerable which resulted in it being unwrapped when returned from controllers

--- ProjectPet.Core/ResponseModels/PagedList.cs
+++ ProjectPet.Core/ResponseModels/PagedList.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
+﻿namespace ProjectPet.Core.ResponseModels;
 
-namespace ProjectPet.Core.ResponseModels;
-
-public class PagedList<T> : IEnumerable<T>
+public class PagedList<T>
 {
     public IReadOnlyList<T>? Data { get; init; } = [];
     public int TotalCount { get; init; }
@@ -10,13 +8,4 @@ public class PagedList<T> : IEnumerable<T>
     public int PageIndex { get; init; }
     public bool HasNextPage => PageSize * PageIndex <= TotalCount;
     public bool HasPreviousPage => PageIndex > 1;
-
-    public IEnumerator<T> GetEnumerator()
-    {
-        return Data!.GetEnumerator();
-    }
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return Data!.GetEnumerator();
-    }
 }
