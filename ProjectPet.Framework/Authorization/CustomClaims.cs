f0c731df9504804c6f7f28320427c875229018f0
brinersam <vladbudyachenko@mail.ru>
2025-08-08 01:42:01 +0300

    Customclaims, userscopeddata

---
+++ ProjectPet.Framework/Authorization/CustomClaims.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectPet.Framework.Authorization;
+
+public static class CustomClaims
+{
+    public const string ROLE = "Role";
+
+    public const string PERMISSION = "Permission";
+
+    public const string ID = "Id";
+}
---
+++ ProjectPet.Framework/Authorization/UserScopedData.cs
@@ -0,0 +1,24 @@
+﻿using ProjectPet.SharedKernel.ErrorClasses;
+
+namespace ProjectPet.Framework.Authorization;
+
+public class UserScopedData
+{
+    public Guid? UserId { get; set; }
+
+    public List<string>? Permissions { get; set; }
+
+    public List<string>? Roles { get; set; }
+
+    public bool IsSuccess => Error is null;
+
+    public Error? Error { get; private set; }
+
+    public void MakeErrored(Error? error = null)
+    {
+        if (error is null)
+            error = Error.Failure("claim.not.found", "Unknown user!");
+
+        Error = error;
+    }
+}
