82d240f8b73669ea9c3a04da36f9e92ae3ba4c82
brinersam <vladbudyachenko@mail.ru>
2025-08-16 01:49:05 +0300

    Update to 1.0.6

---
+++ ProjectPet.Core/Options/RabbitMQOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectPet.Core.Options;
+
+public class RabbitMQOptions
+{
+    public static string REGION = "RabbitMq";
+    public string Host { get; init; }
+    public string Username { get; init; }
+    public string Password { get; init; }
+
+}
---
+++ ProjectPet.SharedKernel/Exceptions/IntegrationEventErroredException.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace ProjectPet.SharedKernel.Exceptions;
+[Serializable]
+public class IntegrationEventErroredException : Exception
+{
+    public IntegrationEventErroredException(string message)
+        : base(message)
+    { }
+
+    protected IntegrationEventErroredException(SerializationInfo info, StreamingContext ctxt)
+        : base(info, ctxt)
+    { }
+}
